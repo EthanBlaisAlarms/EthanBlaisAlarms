7fccaef8558ff58870f64a73b4ee8cccbee78244
EBA <official-eba@outlook.com>
2023-12-11 02:11:40 -0500

    Update Data Files

--- legacy/scripts/hash.js
+++ legacy/scripts/hash.js
@@ -18,32 +18,32 @@ const users = [
     },
     {
         username: "EthanBlaisAlarms",
-        password: 476661430,
+        password: -729038021,
         access: 5
     },
     {
         username: "Access0Test",
-        password: -173674050,
+        password: -729038021,
         access: 0
     },
     {
         username: "Access1Test",
-        password: -173674050,
+        password: -729038021,
         access: 1
     },
     {
         username: "Access2Test",
-        password: -173674050,
+        password: -729038021,
         access: 2
     },
     {
         username: "Access3Test",
-        password: -173674050,
+        password: -729038021,
         access: 3
     },
     {
         username: "Access4Test",
-        password: -173674050,
+        password: -729038021,
         access: 4
     }
 ]
